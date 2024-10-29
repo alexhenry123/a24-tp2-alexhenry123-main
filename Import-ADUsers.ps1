e9c4c5fd38b04e1774d4d4aaef74013666268cc3
alexhenry123 <2134782@cegepmontpetit.ca>
2024-10-29 14:19:38 -0400

    29 oct Commit 2 R1 Sem 9

--- Import-ADUsers.ps1
+++ Import-ADUsers.ps1
@@ -15,17 +15,18 @@
 [CmdletBinding()]
 Param(
     # TODO Paramètres de votre script ici
-    [ValidateScript( { Test-Path $Path } )]
+    [ValidateScript( { Test-Path $_ } )]
     [Parameter(Mandatory, ValueFromPipeline)][string] $Path
 )
 
 Function Write-Log {
     Param(
         # TODO Paramètres de votre fonction de log ici
-        [Parameter(Mandatory)][string] $Chemin_Log
+        [Parameter(Mandatory)][string] $Path_Log
     )
 }
 
+#Arrêter le script s'il y a une erreur
 $ErrorActionPreference = "Stop"
 
 # Importation CSV et assignations de variables
@@ -40,25 +41,37 @@ try {
         $nom_user = ($prenom.substring(0,1) + $nom_famille.substring(0,4)).ToLower()
         $description = $user.NoEmpl
         $mdp = $nom_famille.substring(0,2).ToUpper() + $prenom.substring(0,2).ToLower() + $no_empl
-
-        # Splatting des utilisateurs 
-        $ADUserSplat = @{
-            Name = $nom_user
-            SamAccountName = $nom_user
-            UserPrincipalName = $nom_user
-            GivenName = $prenom
-            Surname = $nom_famille
-            DisplayName = $nom_complet
-            Description = $description
-            Path = Get-ADRootDSE.defaultNamingContext
-            AccountPassword = $mdp | ConvertTo-SecureString -AsPlainText -Force
-            Admin = $admin
-            ChangePasswordAtLogon = $true
-            Enabled = $true
-        }
-        New-ADUser $ADUserSplat
-    } 
+    }
 }
 catch {
     return "Erreur lors de l'importation du CSV"
-}    
+}
+
+# Splatting des utilisateurs 
+try {
+    $ADUserSplat = @{
+        Name = $nom_user
+        SamAccountName = $nom_user
+        UserPrincipalName = $nom_user
+        GivenName = $prenom
+        Surname = $nom_famille
+        DisplayName = $nom_complet
+        Description = $description
+        Path = Get-ADRootDSE.defaultNamingContext
+        AccountPassword = $mdp | ConvertTo-SecureString -AsPlainText -Force
+        Admin = $admin
+        ChangePasswordAtLogon = $true
+        Enabled = $true
+    }
+    New-ADUser $ADUserSplat
+}
+catch {
+    return "Erreur lors de la création de l'utilisateur"
+}
+
+# Conditions de création d'utilisateurs
+
+    # Si l'utilisateur est identifié comme étant administrateur, l'ajouter au groupe d'administrateurs du domaine
+    
+    
+ 
